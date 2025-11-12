f0b372900e311fb072086d840ac3d1a6d38188e9
dwalczak97 <d.melon@op.pl>
2025-11-12 14:17:00 +0100

    id

--- vite-project/src/components/contactForm.jsx
+++ vite-project/src/components/contactForm.jsx
@@ -2,15 +2,18 @@
  import { useId } from "react";
  import { ErrorMessage } from "formik";
  import * as Yup from "yup";
+ import { nanoid } from 'nanoid'
+ 
 
  const initialValues = {
     name: "",
     number: ""
- }
-const ContactForm = () => {
-    const handleSubmit = (values, actions) => {
-		console.log(values);
-		actions.resetForm();
+ };
+
+const ContactForm = ({addContact}) => {
+     const handleSubmit = (values, actions) => {
+		
+      addContact({name: values.name, number: values.number, id:{nanoid} })
 	};
      const nameFieldId = useId();
      const numberFieldId = useId();
@@ -19,17 +22,16 @@ const ContactForm = () => {
         name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
         number: Yup.number().required("Required")
      });
-  return (
+return (
     <Formik 
-    initialValues={initialValues}
-     onSubmit={handleSubmit}
-     validationSchema={FeedbackSchema}>
+    initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}
+      >
     	<Form>
             <label htmlFor={nameFieldId}>Name</label>
-            <Field type="text" name="name" />
+            <Field type="text" name="name" id={nameFieldId} />
             <ErrorMessage name="name" as="span" />
              <label htmlFor={numberFieldId}>Number</label>
-			<Field type="string" name="number" />
+			<Field type="string" name="number" id={numberFieldId} />
             <ErrorMessage name="number" as="span" />
 				<button type="submit">Add Contact</button>
         </Form>
